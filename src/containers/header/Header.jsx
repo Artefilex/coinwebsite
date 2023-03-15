714c60a6dfa705d1efd428dc701f64483f37994c
barış tunçdemir <baris.tncdmr@gmail.com>
2023-03-16 00:06:22 +0300

    header and whatGBT3 files are designed | main branch

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gbt3__header section__padding' id='home'>
+      <div className='gbt3__header-content'>
+        <h1 className='gradient__text'> Let's Build Something amazing with GPT-3 OpenAI</h1>
+        <p>
+          Didn't you know you had that much power? You can move mountains. You can do anything. Trees live in your fan brush, but you have to scare them out. Just think about these things in your mind and drop em' on canvas
+        </p>
+        <div className='gbt3__header-content__input'>
+          <input type="email" placeholder='Your E mail Adress' />
+          <button type='button' > Get Started</button>
+        </div>
+        <div className='gbt3__header-content__people'>
+          <img src={people} alt="people" />
+          <p>1.600 people requested access a visit in last 24 hours</p>
+        </div>
+      
+      </div>
+      <div className='gbt3__header-image'>
+          <img src={ai} alt="ai" />
+        </div>
+      
+    </div>
+
   )
 }
 
--- src/containers/whatGBT3/WhatGBT3.jsx
+++ src/containers/whatGBT3/WhatGBT3.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './whatGBT3.css'
 const WhatGBT3 = () => {
   return (
-    <div>WhatGBT3</div>
+    <div className='gbt3__whatgbt3 section__margin' id='wgpt3'>
+     <div className='gbt3__whatgbt3-feature'>
+      <Feature/>
+     </div>
+     <div className='gbt3__whatgbt3-heading '>
+      <h1 className='gradient-text'> The possibilties are beyond your imagination</h1>
+      <p >  Explore The Library</p>
+     </div>
+     <div className='gbt3__whatgbt3-container '>
+     <Feature/>
+     <Feature/>
+     <Feature/>
+     </div>
+    </div>
   )
 }
 
