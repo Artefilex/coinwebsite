e5d23d080b1c6c79379485753e3a7e6c6373c826
barış tunçdemir <baris.tncdmr@gmail.com>
2023-03-15 04:15:55 +0300

    project files created || main branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
-import './App.css';
-
+import React from 'react'
+import { Footer, Blog, Possibility, WhatGBT3, Header, Features } from './containers'
+import {CTA , Brand, Navbar} from './components'
+import './App.css'
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar/>
+          <Header/>
+        </div>
+         <Brand/>
+         <WhatGBT3/>
+         <Features/>
+         <Possibility/>
+         <CTA/>
+         <Blog/>
+         <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/whatGBT3/WhatGBT3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatGBT3.css'
+const WhatGBT3 = () => {
+  return (
+    <div>WhatGBT3</div>
+  )
+}
+
+export default WhatGBT3
