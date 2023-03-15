a947a6e931fa5b1916381cca8634cc5f996bea2d
barış tunçdemir <baris.tncdmr@gmail.com>
2023-03-15 21:50:47 +0300

    responsive design for navbar || main branch

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import {useState} from 'react'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+
 import './navbar.css'
+import logo from "../../assets/logo.svg"
+// BEM Block Element Modifier
+
+const Menu = () =>(
+  <>
+  <p><a href="#home"> Home</a></p>
+  <p><a href="#wgpt3"> What is GBT3?</a></p>
+  <p><a href="#possibility"> Open AI</a></p>
+  <p><a href="#features"> Case Studies</a></p>
+  <p><a href="#blog"> Library</a></p>
+  </>
+)
+
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
-    <div>Navbar</div>
+    <div className='gbt3__navbar'>
+      <div className='gbt3__navbar-links'>
+        <div className='gbt3__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className='gbt3__navbar-links_container'>
+        <Menu/>
+        </div>
+      </div>
+      <div className='gbt3__navbar-sign'>
+        <p>Sign in</p>
+        <button > Sign up</button>
+      </div>
+      <div className='gbt3__navbar-menu'></div>
+     {toggleMenu  
+     ? <RiCloseLine color = "#fff" size = {27} onClick ={()=> setToggleMenu(false)} /> 
+     : <RiMenu3Line color = "#fff" size = {27} onClick ={()=> setToggleMenu(true)} />
+    }
+    {toggleMenu && (
+      <div className='gbt3__navbar-menu_container scale-up-center'>
+        <div className='gbt3__navbar-menu_container-links'>
+          <Menu/>
+          <div className='gbt3__navbar-menu_container-links_sigin'>
+            <p>Sign in</p>
+        <button > Sign up</button>
+      </div>
+        </div>
+      </div>
+    )}
+
+    </div>
   )
 }
 
