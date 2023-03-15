960f931bde91a46177b7e7dfe0ae39c41e890800
barış tunçdemir <baris.tncdmr@gmail.com>
2023-03-16 01:34:20 +0300

    feature and features are designed | main branch

--- src/containers/whatGBT3/WhatGBT3.jsx
+++ src/containers/whatGBT3/WhatGBT3.jsx
@@ -5,16 +5,16 @@ const WhatGBT3 = () => {
   return (
     <div className='gbt3__whatgbt3 section__margin' id='wgpt3'>
      <div className='gbt3__whatgbt3-feature'>
-      <Feature/>
+      <Feature title= 'What is GPT-3' text="Didn't you know you had that much power? You can move mountains. You can do anything. Trees live in your fan brush, but you have to scare them out." />
      </div>
      <div className='gbt3__whatgbt3-heading '>
-      <h1 className='gradient-text'> The possibilties are beyond your imagination</h1>
+      <h1 className='gradient__text'> The possibilties are beyond your imagination</h1>
       <p >  Explore The Library</p>
      </div>
      <div className='gbt3__whatgbt3-container '>
-     <Feature/>
-     <Feature/>
-     <Feature/>
+     <Feature title='Chatbots' text="Didn't you know you had that much power? You can move mountains. You can do anything. Trees live in your fan brush," />
+     <Feature title='Knowledgebase' text='Didn t you know you had that much power? You can move mountains. You can do anything. Trees live in your fan brush,' />
+     <Feature title='Education' text='Didn t you know you had that much power? You can move mountains. You can do anything. Trees live in your fan brush,'/>
      </div>
     </div>
   )
